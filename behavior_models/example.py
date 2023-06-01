5d0b5505df0484c05e882cd4ca543b2032cecd4b
olivier <olivier.winter@hotmail.fr>
2023-06-01 13:45:11 +0100

    basic unit tests to instantiate model classes

--- behavior_models/example.py
+++ behavior_models/example.py
@@ -1,8 +1,5 @@
 import numpy as np
-from behavior_models.models.expSmoothing_prevAction import expSmoothing_prevAction
-from behavior_models.models.expSmoothing_stimside import expSmoothing_stimside
-from behavior_models.models.optimalBayesian import optimal_Bayesian as CurrentModel
-from behavior_models.models import utils as but
+from behavior_models import utils as but, models
 import pickle
 from one.api import ONE
 import brainbox.io.one as bbone
@@ -15,16 +12,17 @@ except:
     session_idx = 0
     pass
 
+BehaviorModel = models.StimulusKernel
 # ONE
-one = ONE()
+one = ONE(base_url="https://openalyx.internationalbrainlab.org")
 one.alyx.clear_rest_cache()
 bwm_df = bwm_query()
 sessions = bwm_df.eid.unique()
 
 # load session
-session_id = sessions[session_idx]
-subject = bwm_df[bwm_df.eid == session_id].iloc[0].subject
-sess_loader = bbone.SessionLoader(one, session_id)
+eid = sessions[session_idx]
+subject = bwm_df[bwm_df.eid == eid].iloc[0].subject
+sess_loader = bbone.SessionLoader(one, eid)
 if sess_loader.trials.empty:
     sess_loader.load_trials()
 trialsdf = sess_loader.trials
@@ -47,19 +45,19 @@ stim_side, stimuli, actions, pLeft_oracle = but.format_data(data)
 stimuli, actions, stim_side = but.format_input(
     [stimuli], [actions], [stim_side]
 )
-session_uuids = np.array([session_id])
+session_uuids = np.array([eid])
 nb_sessions = len(actions)
 
-# instanciate model
-model = CurrentModel(
-    "results_behavioral/",
-    session_uuids,
-    subject,
-    actions,
-    stimuli,
-    stim_side,
-    single_zeta=False,
-)
+# instantiate model
+model = BehaviorModel(
+        path_to_results="results_behavioral/",
+        session_uuids=session_uuids,
+        mouse_name=subject,
+        actions=actions,
+        stimuli=stimuli,
+        stim_side=stim_side,
+        single_zeta=False,
+    )
 
 # train
 model.load_or_train(remove_old=False, adaptive=True)
